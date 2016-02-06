6f53db4cc8083495858cd5b55ca0858467bb28a6
Chris Hitchcott <hitchcott@gmail.com>
2016-02-06 14:19:06 +0000

    revert loading absolute for now

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "tap:i18n-bundler",
   summary: "Bundles your project's tap-i18n files and pre-loads them on the client automatically",
-  version: '0.2.0',
+  version: '0.3.0',
   git: 'https://github.com/TAPevents/tap-bundler'
 });
 
